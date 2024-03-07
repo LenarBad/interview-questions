876bd31bac86a02a9d0267044d9958edac24d25d
Lenar Badretdinov <LenarBad@users.noreply.github.com>
2024-03-07 14:35:04 -0600

    Create max-path-sum-between-leaves-in-beenary-tree.java

---
+++ trees/max-path-sum-between-leaves-in-beenary-tree.java
@@ -0,0 +1,14 @@
+    private int max = Integer.MIN_VALUE;
+    public int maxSumBeetweenLeaves(TreeNode root) {
+        maxSum(root);
+        return max;
+    }
+
+    private int maxSum(TreeNode node) {
+        if (node == null) return;
+        int leftSum = maxSum(node.left);
+        int rightSum = maxSum(node.right);
+        if (max < leftSum + rightSum + node.val) {
+            max = leftSum + rightSum + node.val;
+        }
+    }
