c45da6a0c7a5342ac46b880c946180e46540b9a4
Lenar Badretdinov <LenarBad@users.noreply.github.com>
2024-03-14 14:25:01 -0500

    Delete misc/custom-comparator.java

--- misc/custom-comparator.java
+++
@@ -1,7 +0,0 @@
-class NodeComparator implements Comparator<ListNode> {
-    public int compare(ListNode a, ListNode b) {
-        return a.val - b.val;
-    }
-}
-
-new PriorityQueue<ListNode>(new NodeComparator());
