f6509c4d265f9831408ce4e8577c42091aabc37a
Lenar Badretdinov <LenarBad@users.noreply.github.com>
2024-03-18 18:29:53 -0500

    Create 3sum.java

---
+++ arrays/3sum.java
@@ -0,0 +1,70 @@
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+*/
+
+public List<List<Integer>> threeSum(int[] nums) {
+    Arrays.sort(nums);
+    List<List<Integer>> arr = new ArrayList<>();
+
+    for (int i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] == nums[i + 1]) {
+            continue;
+        }
+        int j = i + 1;
+        int k = nums.length - 1;
+        while (j < k) {
+            if (nums[i] + nums[j] + nums[k] == 0) {
+                arr.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                j = skipDuplicates(j, k, nums, true);
+                k = skipDuplicates(j, k, nums, false);
+                j++;
+                k--;
+            } else {
+                if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+    }
+    return arr;
+}
+private int skipDuplicates(int left, int right, int[] nums, boolean up) {
+    if (up) {
+        while (left < right && nums[left] == nums[left + 1]) {
+            left++;
+        }
+        return left;
+    } else {
+        while (left < right && nums[right] == nums[right - 1]) {
+            right--;
+        }   
+        return right;        
+    }
+} 
