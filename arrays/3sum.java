71f46836af084f4d6f6417a561de4f045dcba6cf
Lenar Badretdinov <LenarBad@users.noreply.github.com>
2024-03-18 18:30:37 -0500

    Update 3sum.java

--- arrays/3sum.java
+++ arrays/3sum.java
@@ -32,7 +32,7 @@ public List<List<Integer>> threeSum(int[] nums) {
     List<List<Integer>> arr = new ArrayList<>();
 
     for (int i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] == nums[i + 1]) {
+        if (i > 0 && nums[i] == nums[i - 1]) {
             continue;
         }
         int j = i + 1;
